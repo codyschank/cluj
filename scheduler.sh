8b373bd70b0876c40333315d19d709a52438bcb0
Cody Schank <codyschank@gmail.com>
2020-01-30 21:26:08 -0600

    Daily scheduled job

--- scheduler.sh
+++ scheduler.sh
@@ -1,3 +1,6 @@
-PATH=/Users/codyschank/anaconda3/bin
+PATH=/Users/codyschank/anaconda3/bin:/usr/local/bin
 jupyter nbconvert --to notebook --execute /Users/codyschank/cluj/yesterdays_boxscores.ipynb
-jupyter nbconvert --to notebook --execute /Users/codyschank/cluj/Simulate_matchup_results.ipynb
+jupyter nbconvert --ExecutePreprocessor.timeout=6000 --to notebook --execute /Users/codyschank/cluj/Simulate_matchup_results.ipynb
+git add -A
+git commit -m 'Daily scheduled job'
+git push --set-upstream origin master
